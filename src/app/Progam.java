838b14135fd9118fa1158d4dfeeb59e173af9125
José Carlos Duque Filho <cj_duque@hotmail.com>
2020-12-27 20:10:48 -0300

    Created entities and concluded

--- src/app/Progam.java
+++ src/app/Progam.java
@@ -1,10 +1,57 @@
 package app;
 
+import java.util.Locale;
+import java.util.Scanner;
+import entities.ContaBancaria;
+
 public class Progam {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+
+		ContaBancaria conta;
+		double deposit;
+
+		System.out.print("Enter account number: ");
+		int account = sc.nextInt();
+
+		System.out.print("Enter account holder: ");
+		sc.nextLine();
+		String name = sc.nextLine();
+		System.out.print("Is there na initial deposit (y/n)? ");
+		char thereDeposit = sc.next().charAt(0);
+
+		if (thereDeposit == 'y') {
+			System.out.print("Enter initial deposit value: ");
+			deposit = sc.nextDouble();
+			conta = new ContaBancaria(account, name, deposit);
+			System.out.println("Account data:");
+			System.out.println(conta);
+
+		} else {
+			conta = new ContaBancaria(account, name);
+			System.out.println("Account data:");
+			System.out.println(conta);
+		}
+		System.out.println();
+	
+
+		System.out.print("Enter a deposit value: ");
+		deposit = sc.nextDouble();
+		conta.depositValue(deposit);
+		System.out.println("Updated account data:");
+		System.out.println(conta);
+		System.out.println();
+
+		System.out.print("Enter a withdraw value: ");
+		deposit = sc.nextDouble();
+		conta.withdrawValue(deposit);
+		System.out.println("Updated account data:");
+		System.out.println(conta);
+
+		sc.close();
 	}
 
 }
--- src/entities/ContaBancaria.java
+++ src/entities/ContaBancaria.java
@@ -1,5 +1,58 @@
 package entities;
 
-public class ContaBancaria {
 
+
+public class ContaBancaria {
+	
+	private int account;
+	private	String name;
+	private  double deposit;
+	
+	public ContaBancaria() {
+		
+	}
+	
+	public ContaBancaria(int account, String name, double deposit) {
+	
+		this.account = account;
+		this.name = name;
+		this.deposit = deposit;
+	}
+	
+	public ContaBancaria(int account, String name) {
+		this.account = account;
+		this.name = name;
+	}
+	
+	public int getAccount() {
+		return account;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public double getDeposit() {
+		return deposit;
+	}
+
+	public void depositValue(double deposit) {
+		this.deposit = this.deposit + deposit;
+	}
+	
+	public void withdrawValue(double deposit) {
+		
+		this.deposit = this.deposit - (deposit + 5.0);
+	}
+	
+	public String toString() {
+		return
+				"Account " 
+				+ account
+				+ ", Holder: "
+				+ name 
+				+ ", Balance: $ "
+				+ String.format("%.2f", deposit);
+	}
 }
+	
